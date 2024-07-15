b0da0b035fc1a19c378b9c50ac4aa95c389aac60
Plecaj <michalkalasiewicz0@gmail.com>
2024-07-15 17:03:43 +0200

    Made triangle using OpenGL

--- RodEngine/src/Platform/OpenGL/OpenGLContext.cpp
+++ RodEngine/src/Platform/OpenGL/OpenGLContext.cpp
@@ -14,6 +14,11 @@ namespace Rod {
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		RD_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		RD_CORE_INFO("OpenGL Info:");
+		RD_CORE_INFO("	Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		RD_CORE_INFO("	Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		RD_CORE_INFO("	Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()

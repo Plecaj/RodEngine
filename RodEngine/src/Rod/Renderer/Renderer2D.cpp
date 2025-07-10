6d9f9d49cb1b7fa31d589373c70f782863eff488
Plecaj <michalkalasiewicz0@gmail.com>
2025-07-10 16:44:21 +0200

    Renderer 2D now uses just one, two in one shader instead of two shaders

--- RodEngine/src/Rod/Renderer/Renderer2D.cpp
+++ RodEngine/src/Rod/Renderer/Renderer2D.cpp
@@ -14,8 +14,8 @@ namespace Rod {
     struct Renderer2DStorage 
     {
         Ref<VertexArray> QuadVertexArray;
-        Ref<Shader> FlatColorShader;
         Ref<Shader> TextureShader;
+        Ref<Texture2D> WhiteTexture;
     };
 
     static Renderer2DStorage* s_Data;
@@ -45,8 +45,11 @@ namespace Rod {
         squareIB = IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
         s_Data->QuadVertexArray->SetIndexBuffer(squareIB);
 
-        s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
-        s_Data->TextureShader = Shader::Create("assets/shaders/Texture.glsl");
+        s_Data->WhiteTexture = Texture2D::Create(1, 1);
+        uint32_t whiteTextureData = 0xffffffff;
+        s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(whiteTextureData));
+
+        s_Data->TextureShader = Shader::Create("../RodEngine/assets/shaders/Texture.glsl");
         s_Data->TextureShader->Bind();
         s_Data->TextureShader->SetInt("u_Texture", 0);
     }
@@ -58,9 +61,6 @@ namespace Rod {
 
     void Renderer2D::BeginScene(const OrthographicCamera& camera)
     {
-        s_Data->FlatColorShader->Bind();
-        s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
         s_Data->TextureShader->Bind();
         s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
     }
@@ -76,13 +76,11 @@ namespace Rod {
 
     void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color, float rotation)
     {
-        // Should be cashed in begin scene
-        s_Data->FlatColorShader->Bind();
-        //
-        s_Data->FlatColorShader->SetFloat4("u_Color", color);
+        s_Data->TextureShader->SetFloat4("u_Color", color);
+        s_Data->WhiteTexture->Bind();
 
         glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f }) * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
-        s_Data->FlatColorShader->SetMat4("u_Transform", transform);
+        s_Data->TextureShader->SetMat4("u_Transform", transform);
 
         s_Data->QuadVertexArray->Bind();
         RenderCommand::DrawIndexed(s_Data->QuadVertexArray);
@@ -95,9 +93,7 @@ namespace Rod {
 
     void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float rotation)
     {
-        // Should be cashed in begin scene
-        s_Data->TextureShader->Bind();
-        //
+        s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(1.0f));
         glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f }) * glm::rotate(glm::mat4(1.0f), glm::radians(rotation), glm::vec3(0.0f, 0.0f, 1.0f));
         s_Data->TextureShader->SetMat4("u_Transform", transform);
 
